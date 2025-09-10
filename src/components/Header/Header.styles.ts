4ef486694bff3a474a2bfd1b4422b30953417c8c
Raj Sarkar <raj.sarkar@joshtechnologygroup.com>
2025-09-10 19:13:02 +0530

    [RS_A2_01]: Made header sticky

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -5,6 +5,8 @@ export const StyledHeaderStack = styled(Stack)(({ theme }) => ({
     justifyContent: 'space-between',
     padding: theme.spacing(4),
     maxHeight: theme.typography.pxToRem(70),
+    position: 'sticky',
+    top: 0,
     [theme.breakpoints.up('sm')]: {
         padding: `${theme.spacing(3)} ${theme.spacing(5)}`,
     },
